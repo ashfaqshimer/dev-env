30fa7222680dab3c045a9a819ad10e01180993dc
ashfaqshimer <shimer.ahamed@gmail.com>
2021-04-20 22:42:05 +0530

    Develop login and register components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
-	return <div>Login</div>;
+	const [formData, setFormData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const { email, password } = formData;
+
+	const handleChange = (evt) =>
+		setFormData({ ...formData, [evt.target.name]: evt.target.value });
+
+	const handleSubmit = async (evt) => {
+		evt.preventDefault();
+
+		console.log('SUCCESS!');
+	};
+
+	return (
+		<>
+			<div className='alert alert-danger'>Invalid credentials</div>
+			<h1 className='large text-primary'>Sign In</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Sign into Your Account
+			</p>
+			<form className='form' onSubmit={handleSubmit}>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email Address'
+						value={email}
+						name='email'
+						required
+						onChange={handleChange}
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						value={password}
+						name='password'
+						onChange={handleChange}
+					/>
+				</div>
+				<input type='submit' className='btn btn-primary' value='Login' />
+			</form>
+			<p className='my-1'>
+				Don't have an account? <Link to='/register'>Sign Up</Link>
+			</p>
+		</>
+	);
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
-	return <div>Register</div>;
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '',
+	});
+
+	const { name, email, password, password2 } = formData;
+
+	const handleChange = (evt) =>
+		setFormData({ ...formData, [evt.target.name]: evt.target.value });
+
+	const handleSubmit = async (evt) => {
+		evt.preventDefault();
+		if (password !== password2) {
+			console.log('Passwords do not match');
+		} else {
+			console.log('SUCCESS!');
+		}
+	};
+
+	return (
+		<>
+			<h1 className='large text-primary'>Sign Up</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Create Your Account
+			</p>
+			<form className='form' onSubmit={handleSubmit}>
+				<div className='form-group'>
+					<input
+						type='text'
+						placeholder='Name'
+						name='name'
+						value={name}
+						required
+						onChange={handleChange}
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email Address'
+						value={email}
+						name='email'
+						onChange={handleChange}
+					/>
+					<small className='form-text'>
+						This site uses Gravatar so if you want a profile image, use a
+						Gravatar email
+					</small>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						value={password}
+						name='password'
+						minLength='6'
+						onChange={handleChange}
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Confirm Password'
+						value={password2}
+						name='password2'
+						minLength='6'
+						onChange={handleChange}
+					/>
+				</div>
+				<input type='submit' className='btn btn-primary' value='Register' />
+			</form>
+			<p className='my-1'>
+				Already have an account? <Link to='/login'>Sign In</Link>
+			</p>
+		</>
+	);
 };
 
 export default Register;
